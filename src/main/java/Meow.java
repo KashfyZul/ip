eed8b58aa19577407b98f22561dcdf61b8287050
KashfyZul <e1121648@u.nus.edu>
2025-02-04 21:17:36 +0800

    Changed "Duke" to "Meow" Added ASCII art of a cat

---
+++ src/main/java/Meow.java
@@ -0,0 +1,20 @@
+public class Meow {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String chatbotName = "Meow";
+
+        String meow =" /\\_/\\ \n"
+        + "( o.o ) \n"
+        + " > ^ < \n";
+
+        System.out.println("Hello from\n \n" + meow + chatbotName);
+        System.out.println("What can I do for you? Meow.");
+        System.out.println("_____________________________________");
+        System.out.println("Bye. See you!. Meow.");
+    }
+}
